8348f3b3575d3eee060f7c70661d171d4cdaeece
Mbassi Japhet <mbassijaphet@gmail.com>
2021-11-07 12:17:21 +0100

    Converted the 'new contact' form to a reactive form
    Added validation the the name field

--- src/app/contact-manager/components/new-contact-dialog/new-contact-dialog.component.ts
+++ src/app/contact-manager/components/new-contact-dialog/new-contact-dialog.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Note } from '../../models/note';
 import { User } from '../../models/user';
 
 @Component({
@@ -10,15 +12,29 @@ import { User } from '../../models/user';
 export class NewContactDialogComponent implements OnInit {
 
   user!: User;
+  newContactForm!: FormGroup;
   avatars = ["svg-1", "svg-2", "svg-3", "svg-4"];
-  
+  name = new FormControl('', [Validators.required]);
+  avatar = new FormControl('');
+  bio = new FormControl('');
+
   constructor(private dialogRef: MatDialogRef<NewContactDialogComponent>) { }
+
+  getErrorMessage() {
+    return this.name.hasError('required') ? 'You must enter a name' : null;
+  }
 
   ngOnInit(): void {
     this.user = new User();
+    this.newContactForm = new FormGroup({
+      avatar: this.avatar,
+      name: this.name,
+      bio: this.bio,
+      }
+    );
   }
 
   save() {
-    this.dialogRef.close(this.user);
+    this.dialogRef.close(this.newContactForm.value);
   }
 }
--- src/app/contact-manager/contact-manager.module.ts
+++ src/app/contact-manager/contact-manager.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ContactManagerAppComponent } from './contact-manager-app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
@@ -28,6 +28,7 @@ import { NewContactDialogComponent } from './components/new-contact-dialog/new-c
     CommonModule,
     MaterialModule,
     HttpClientModule,
+    ReactiveFormsModule,
     ContactManagerRoutingModule,
   ],
   entryComponents: [
